88490b69b4bbe25324670a84028140e315ec8acb
Mishkov,Angel A.Y <a.mishkov@student.fontys.nl>
2019-02-24 20:04:45 +0100

    CRUD for array of Departments

--- Web2-Project/src/app/app.module.ts
+++ Web2-Project/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule }from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DepartmentsComponent } from './departments/departments.component';
@@ -12,7 +12,8 @@ import { DepartmentsComponent } from './departments/departments.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Web2-Project/src/app/departments/departments.component.ts
+++ Web2-Project/src/app/departments/departments.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {DEPARTMENTS} from '../mock-departments';
+import { Department } from '../department';
 @Component({
   selector: 'app-departments',
   templateUrl: './departments.component.html',
@@ -8,9 +9,23 @@ import {DEPARTMENTS} from '../mock-departments';
 export class DepartmentsComponent implements OnInit {
 
  departments=DEPARTMENTS;
+ selectedDepartment: Department;
+ name="";
+ id=90;
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(department:Department):void{
+    this.selectedDepartment=department;
+  }
+  onClick(name){
+    this.departments.push(new Department(this.id=this.id+10,name))
+  }
+  onRemove(department:Department):void{
+    const index = this.departments.indexOf(department);
+    this.departments.splice( index,1);
+    
+  }
 }
