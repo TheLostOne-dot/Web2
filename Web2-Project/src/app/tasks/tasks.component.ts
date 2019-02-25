ebcdcfa992fcdac20d23a8112bcef9f30d698430
Станимир (Stanimir) <stanimir.nikolov@student.fontys.nl>
2019-02-25 11:20:11 +0100

    Week 3 Assignment Tasks

--- Web2-Project/src/app/tasks/tasks.component.ts
+++ Web2-Project/src/app/tasks/tasks.component.ts
@@ -10,10 +10,26 @@ import { Tasks } from '../mock-tasks';
 export class TasksComponent implements OnInit {
 
   tasks = Tasks;
+  selectedTask:Task;
+  name="";
+  id=15;
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  onSelect(task:Task):void{
+    this.selectedTask=task;
+  }
+  onClick(name){
+    this.tasks.push(new Task(this.id=this.id+1,name))
+  }
+  onRemove(task:Task):void{
+    const index = this.tasks.indexOf(task);
+    this.tasks.splice( index,1);
+    
+  }
+
 
 }
