ad569e9491657422b3408ce76ef42383ad5fedb7
Станимир (Stanimir) <stanimir.nikolov@student.fontys.nl>
2019-03-11 00:01:08 +0100

    *bugfixes*

--- Web2-Project/src/app/departments/departments.component.ts
+++ Web2-Project/src/app/departments/departments.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {DEPARTMENTS} from '../mock-departments';
 import { Department } from '../department';
 import {DepartmentService} from '../department.service';
+import { Employee } from '../employees';
+import { EmployeesService } from '../employees.service';
 
 @Component({
   selector: 'app-departments',
@@ -9,12 +11,12 @@ import {DepartmentService} from '../department.service';
   styleUrls: ['./departments.component.css']
 })
 export class DepartmentsComponent implements OnInit {
-
+ selectedEmployees: Employee[];
  departments: Department[];
  selectedDepartment: Department;
  name="";
  id=90;
-  constructor(private departmentService: DepartmentService) { }
+  constructor(private departmentService: DepartmentService, private employeesService: EmployeesService) { }
 getDepartments():void{
   this.departmentService.getDepartments()
   .subscribe(departments => this.departments=departments);
@@ -25,6 +27,22 @@ getDepartments():void{
 
   onSelect(department:Department):void{
     this.selectedDepartment=department;
+    this.selectedEmployees=[];
+    this.employeesService.getEmployees()
+      .subscribe (employee => 
+        {
+          for(let i=0; i<department.employees.length;i++)
+          {
+            for(let j=0; j<employee.length;j++)
+              {
+                if(department.employees[i]==employee[j].id)
+                {
+                      this.emps.push(empl[j]);
+                }
+              }
+          }
+        }
+      )
   }
   onClick(name){
     this.departments.push(new Department(this.id=this.id+10,name))
