f1dfc127f4d28fe8764cb134460c211123b86007
Станимир (Stanimir) <stanimir.nikolov@student.fontys.nl>
2019-03-10 20:04:58 +0100

    removed unnecessary libraries from task service

--- Web2-Project/src/app/tasks.service.ts
+++ Web2-Project/src/app/tasks.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Task } from './task';
 import { Tasks } from './mock-tasks';
-import { MAX_LENGTH_VALIDATOR } from '@angular/forms/src/directives/validators';
-import { identifierModuleUrl } from '@angular/compiler';
 
 @Injectable({
   providedIn: 'root'
