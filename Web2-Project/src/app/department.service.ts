0384b72339c7bbd5b453087ff49d730d16eb7c1b
Mishkov,Angel A.Y <a.mishkov@student.fontys.nl>
2019-03-24 23:11:55 +0100

    api search for every component

--- Web2-Project/src/app/department.service.ts
+++ Web2-Project/src/app/department.service.ts
@@ -4,18 +4,47 @@ import { Observable, of } from 'rxjs';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
-
+import { catchError, map, tap } from 'rxjs/operators';
+import { MessageService } from './messeage.service'
 @Injectable({
   providedIn: 'root'
 })
 export class DepartmentService {
-
-  constructor(private http:HttpClient,private router:Router) { }
+  private departmentsUrl = 'http://i875395.hera.fhict.nl/api/386275/department';  // URL to web api
+  constructor(private http:HttpClient,private router:Router,private messageService: MessageService) { }
   getDepartments(): Observable<Department[]> {
     return this.http.get<Department[]>('http://i875395.hera.fhict.nl/api/386275/department');
   }
   getDepartment(id: number): Observable<Department>{
    
     return this.http.get<Department>('http://i875395.hera.fhict.nl/api/386275/department?id=' + id);
+  }
+
+
+  searchDepartments(term: string): Observable<Department[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.http.get<Department[]>(`${this.departmentsUrl}/?name=${term}`).pipe(
+      tap(_ => this.log(`found departmetns matching "${term}"`)),
+      catchError(this.handleError<Department[]>('searchDepartments', []))
+    );
+  }
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+ 
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+ 
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+ 
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
--- Web2-Project/src/app/employees.service.ts
+++ Web2-Project/src/app/employees.service.ts
@@ -4,14 +4,15 @@ import { Observable, of } from 'rxjs';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router, Route } from '@angular/router';
-
+import { catchError, map, tap } from 'rxjs/operators';
+import { MessageService } from './messeage.service'
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeesService {
-  constructor(private http: HttpClient, private router: Router) { }
-
+  constructor(private http: HttpClient, private router: Router,private messageService: MessageService) { }
+  private employeesUrl = 'http://i875395.hera.fhict.nl/api/386275/employee';  // URL to web api
   getEmployees(): Observable<Employee[]> {
     return this.http.get<Employee[]>('http://i875395.hera.fhict.nl/api/386275/employee');
   }
@@ -22,5 +23,30 @@ export class EmployeesService {
   }
 
   
-
+  searchEmployees(term: string): Observable<Employee[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.http.get<Employee[]>(`${this.employeesUrl}/?name=${term}`).pipe(
+      tap(_ => this.log(`found employees matching "${term}"`)),
+      catchError(this.handleError<Employee[]>('searchEmployees', []))
+    );
+  }
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+ 
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+ 
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+ 
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
 }
