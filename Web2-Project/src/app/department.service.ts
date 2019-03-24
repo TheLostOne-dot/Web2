d3f4664b82a17873429add4d9429c84d508659b4
Станимир (Stanimir) <stanimir.nikolov@student.fontys.nl>
2019-03-24 18:39:01 +0100

    changed the hera address

--- Web2-Project/src/app/department.service.ts
+++ Web2-Project/src/app/department.service.ts
@@ -12,10 +12,10 @@ export class DepartmentService {
 
   constructor(private http:HttpClient,private router:Router) { }
   getDepartments(): Observable<Department[]> {
-    return this.http.get<Department[]>('http://i875395.hera.fhict.nl/api/department/read.php');
+    return this.http.get<Department[]>('http://i386275.hera.fhict.nl/api/department/read.php');
   }
   getDepartment(id: number): Observable<Department>{
    
-    return this.http.get<Department>('http://i875395.hera.fhict.nl/api/387277/department?id=' + id);
+    return this.http.get<Department>('http://i386275.hera.fhict.nl/api/387277/department?id=' + id);
   }
 }
