d1c19ac1df8cf9bdf467df2dce2cf63a143ebf19
i387149 <i387149@student.fontys.nl>
2019-03-25 04:25:48 +0100

    Not fully functional website

--- Web2-Project/src/app/tasks.service.ts
+++ Web2-Project/src/app/tasks.service.ts
@@ -10,7 +10,7 @@ import { Router, Route } from '@angular/router';
 })
 export class TasksService {
 
-  constructor(private http: HttpClient, private roouter: Router) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   getTasks(): Observable<Task[]> {
     return this.http.get<Task[]>('http://i875395.hera.fhict.nl/api/386275/task');
@@ -18,5 +18,18 @@ export class TasksService {
 
   getTask(id: number): Observable<Task>{
     return this.http.get<Task>('http://i875395.hera.fhict.nl/api/386275/task?id=' + id);
+  }
+
+  addTask(task: Task): void {
+    this.http.post<Task>('http://i875395.hera.fhict.nl/api/386275/task', JSON.stringify(task)).subscribe();
+  }
+
+  deleteTask(task: Task): void {
+    this.http.delete<Task>('http://i875395.hera.fhict.nl/api/386275/task?id=' + task.id).
+      subscribe(any => this.router.navigateByUrl('/task'));
+  }
+
+  updateTask(task: Task): void {
+    this.http.put<Task>('http://i875395.hera.fhict.nl/api/386275/task', JSON.stringify(task)).subscribe();
   }
 }
--- Web2-Project/src/app/tasks/tasks.component.ts
+++ Web2-Project/src/app/tasks/tasks.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Task } from '../task';
 import { TasksService } from '../tasks.service';
 import { NgbDate } from '@ng-bootstrap/ng-bootstrap';
+import { DepartmentService } from '../department.service';
+import { Department } from '../department';
 
 @Component({
   selector: 'app-tasks',
@@ -11,20 +13,25 @@ import { NgbDate } from '@ng-bootstrap/ng-bootstrap';
 export class TasksComponent implements OnInit {
 
   tasks: Task[];
+  departments: Department[];
   selectedTask: Task;
   name="";
   id = 20;
   startdate = new NgbDate(2019,1,12);
-  endDate = new NgbDate(2019,12,12);
-  constructor(private taskService: TasksService) { }
+  endDate = new NgbDate(2019, 12, 12);
+  constructor(private taskService: TasksService, private departmentService: DepartmentService) { }
 
   getTasks(): void {
     this.taskService.getTasks()
       .subscribe(tasks => this.tasks = tasks);
-}
-
+  }
+  getDepartments(): void {
+    this.departmentService.getDepartments()
+      .subscribe(departments => this.departments = departments);
+  }
   ngOnInit() {
     this.getTasks();
+    this.getDepartments();
   }
 
   onSelect(task: Task): void {
